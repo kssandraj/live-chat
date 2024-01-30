36276c297099e597613714ee19f9c1e19665ca58
Cassandra Juif <cassandra.juif@gmail.com>
2024-01-30 14:03:08 +0100

    feat: adding the accept and deny friend request functionnalities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,3 @@
-// import { FC } from "react";
-// import Button from "../components/ui/Button";
-
-// interface pageProps {}
-
-// const page: FC<pageProps> = ({}) => {
-//   return <Button>Hello</Button>;
-// };
-
-// export default page;
-
-import { db } from "@/lib/db";
-
 export default async function Home() {
   return <div className="text-red-500">Hello World !</div>;
 }
